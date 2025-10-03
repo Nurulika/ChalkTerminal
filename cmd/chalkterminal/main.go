3cf0c88d9a27f632ca5207657cbd63ed2597434c
Nuru <ceruks@eteb.me>
2025-10-03 20:50:50 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/chalkterminal/main.go
@@ -0,0 +1,23 @@
+// cmd/chalkterminal/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "chalkterminal/internal/chalkterminal"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := chalkterminal.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
